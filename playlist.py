b69aa4b1103a3a64c0f08561f9de07a075bc1674
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-11-20 13:25:39 -0600

    Se quito la libreria de consola con colores

--- playlist.py
+++ playlist.py
@@ -1,46 +1,41 @@
 from pytube import Playlist, YouTube
-from termcolor import colored, cprint
-from colorama import Fore, Back, Style
 
-cprint(" ► ", 'white', 'on_red', end=' ')
-cprint("YOUTUBE PLAYLIST DOWNLOADER\n", attrs=['bold'])
-playlist_url = input("Pege el link de la playlist: " + Fore.BLUE)
-print(Style.RESET_ALL)
+print("► YOUTUBE PLAYLIST DOWNLOADER\n")
+playlist_url = input("Pege el link de la playlist: ")
 
 playlist = Playlist(playlist_url)
+no_descargados = []
 
 try:
     cantidad = len(playlist)
     nombre_lista = str(playlist.title)
-    cprint(Fore.BLACK + Back.WHITE + "Playlist " + nombre_lista, attrs=['bold'])
-    print(Style.RESET_ALL)
-    # print(str(playlist.title) + '\n')
+    print("Playlist " + nombre_lista)
 except Exception as e:
-    cprint("\n\tError!",'red',attrs=['bold'], end=' ')
-    cprint("No se pudo obtener la lista " + str(e))
+    print("\n\tError!", end=' ')
+    print("No se pudo obtener la lista " + str(e))
 else:
     for video_url in playlist:
         try:
             video = YouTube(video_url)
         except Exception as e:
-            cprint("Error!",'red',attrs=['bold'], end=' ')
+            print("Error!", 'red', end=' ')
             print("No se pudo obtener video: " + video_url)
             print(str(e))
         else:
-            print("\n\t" + Fore.BLACK + Back.WHITE + video.title)
-            print(Style.RESET_ALL)
+            print("\n\t" + video.title)
             try:
                 # obtener stream de mejor calidad
                 stream = video.streams.filter(mime_type="video/mp4", progressive="True")[-1]
             except Exception as e:
-                cprint("Error!",'red',attrs=['bold'], end=' ')
+                print("Error!", end=' ')
                 print("\tNo se pudo acceder al stream del video" + str(e))
             else:
-                cprint("descargando...", 'green')
+                print("descargando...")
                 print(str(stream))
                 try:
                     stream.download('downloads/')
                 except Exception as e:
-                    cprint("Fallo la descarga", 'red')
+                    print("Fallo la descarga")
+                    no_descargados.append(video.title)
                 else:
-                    cprint("...completado!", 'blue')
+                    print("...completado!")
